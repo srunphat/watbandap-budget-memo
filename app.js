4e41569f64c8592768a807f57fa0aca62655c660
srunphat <srunphutza@gmail.com>
2026-07-26 19:14:24 +0700

    UP26

--- app.js
+++ app.js
@@ -1449,7 +1449,7 @@ const app = {
                                 <td style="text-align: center;">${this.toThaiNumerals(index + 1)}</td>
                                 <td style="text-align: left;">
                                     ${act.name || "-"}
-                                    ${isCurrent ? `<div style="font-size: 15pt; color: #555555; font-weight: normal; margin-top: 4px;">(กิจกรรมที่เสนออนุมัติครั้งนี้)</div>` : ""}
+                                    ${isCurrent ? `<div style="font-size: 14pt; color: #555555; font-weight: normal; margin-top: 4px;">(กิจกรรมที่เสนออนุมัติครั้งนี้)</div>` : ""}
                                 </td>
                                 <td style="text-align: right;">${this.toThaiNumerals(this.formatCurrency(act.budget))}</td>
                                 <td style="text-align: left;">${act.owner || "-"}</td>
